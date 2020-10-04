6b01b4101294415c3dfc097ee68c8f9fe11575ac
Davi Furtado <44845457+davifurtado@users.noreply.github.com>
2020-10-04 18:47:36 -0300

    remove overflow from characterlist

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -64,7 +64,7 @@ const CharacterList = () => {
                         }
                     </div>
                 ) : (
-                    <div style={{ height: '500px', overflow: 'auto' }}> 
+                    <div> 
                         <FinalCharacter />
                     </div>
                     )
